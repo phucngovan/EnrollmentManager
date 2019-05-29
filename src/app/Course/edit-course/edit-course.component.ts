f09bf9a44cf27331dc9a701ea327b0f7dc033bd5
phucngovan <phucdepzai0712@gmail.com>
2019-05-29 20:13:11 +0700

    CRUD-course

--- src/app/Course/edit-course/edit-course.component.ts
+++ src/app/Course/edit-course/edit-course.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Router} from '@angular/router';
+import {CourseService} from '../../course.service';
+import {first} from 'rxjs/operators';
 
 @Component({
   selector: 'app-edit-course',
@@ -6,10 +10,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-course.component.css']
 })
 export class EditCourseComponent implements OnInit {
+  submitted = false;
+  editForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
+  get f() {
+    return this.editForm.controls;
   }
 
+  constructor(private formBuilder: FormBuilder, private router: Router, private courseService: CourseService) { }
+
+  ngOnInit() {
+    this.editForm = this.formBuilder.group({
+      id: [''],
+      name: ['', Validators.required]
+    });
+    const courseId = localStorage.getItem('editCourseId');
+    if (!courseId) {
+      alert('Invalid action.');
+      this.router.navigate(['list-course']);
+      return;
+    }
+    this.courseService.getCourseId(courseId)
+      .subscribe( data => {
+        this.editForm.setValue(data);
+      });
+  }
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.editForm.invalid) {
+      return;
+    }
+    this.courseService.editCourse(this.editForm.value)
+      .pipe(first())
+      .subscribe(
+        data => {
+          this.router.navigate(['list-course']);
+        },
+        error => {
+          alert(error);
+        });
+  }
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,9 @@ import { Routes, RouterModule } from '@angular/router';
 import {ListStudentComponent} from './Student/list-student/list-student.component';
 import {AddStudentComponent} from './Student/add-student/add-student.component';
 import {EditStudentComponent} from './Student/edit-student/edit-student.component';
+import {ListCourseComponent} from './Course/list-course/list-course.component';
+import {AddCourseComponent} from './Course/add-course/add-course.component';
+import {EditCourseComponent} from './Course/edit-course/edit-course.component';
 
 const routes: Routes = [
   {path: 'list-student',
@@ -14,6 +17,18 @@ const routes: Routes = [
   {
     path: 'edit-student',
     component: EditStudentComponent
+  },
+  {
+    path: 'list-course',
+    component: ListCourseComponent
+  },
+  {
+    path: 'add-course',
+    component: AddCourseComponent,
+  },
+  {
+    path: 'edit-course',
+    component: EditCourseComponent
   }
 ];
 
