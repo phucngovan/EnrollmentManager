77b75580f4f7c95cf831d55ac67ff07d1801a300
phucngovan <phucdepzai0712@gmail.com>
2019-05-29 16:19:40 +0700

    getAllStudent+Add+valid regex

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ListStudentComponent} from './Student/list-student/list-student.component';
+import {AddStudentComponent} from './Student/add-student/add-student.component';
+import {EditStudentComponent} from './Student/edit-student/edit-student.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'list-student',
+  component: ListStudentComponent},
+  {
+    path: 'add-student',
+    component: AddStudentComponent
+  },
+  {
+    path: 'edit-student',
+    component: EditStudentComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
